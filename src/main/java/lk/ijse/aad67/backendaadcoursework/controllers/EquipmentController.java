7ae2a993a8423bced32ea71c17dac88b1ad45c81
DILSHAN <dilshanfonseka76@gmail.com>
2024-12-05 00:29:12 +0530

    equipment controller added

---
+++ src/main/java/lk/ijse/aad67/backendaadcoursework/controllers/EquipmentController.java
@@ -0,0 +1,10 @@
+package lk.ijse.aad67.backendaadcoursework.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/equipment")
+public class EquipmentController {
+
+}
