21a486ae55aed7b0a55ca116cabdb8140405909c
DILSHAN <dilshanfonseka76@gmail.com>
2024-12-04 14:28:03 +0530

    excetions added

--- src/main/java/lk/ijse/aad67/backendaadcoursework/dao/EquipmentDao.java
+++ src/main/java/lk/ijse/aad67/backendaadcoursework/dao/EquipmentDao.java
@@ -2,7 +2,9 @@ package lk.ijse.aad67.backendaadcoursework.dao;
 
 import lk.ijse.aad67.backendaadcoursework.dto.impl.EquipmentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EquipmentDao extends JpaRepository<EquipmentDto,String> {
 
 }
